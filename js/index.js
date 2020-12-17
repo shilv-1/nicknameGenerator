260868a081d9256f36903eb5c5923b1c82df23bd
shilv-1 <shilv_666@163.com>
2020-12-17 19:32:47 +0800

    第一次提交

---
+++ js/index.js
@@ -0,0 +1,28 @@
+(function(){
+    // 1.获取输入的1-6位昵称
+    var input01 = document.getElementById("input01");
+    var input02 = document.getElementById("input02");
+    var input03 = document.getElementById("input03");
+    var input04 = document.getElementById("input04");
+    var input05 = document.getElementById("input05");
+    var input06 = document.getElementById("input06");
+    // 2.获取按钮
+    var specialBtn = document.querySelector(".specialBtn");
+    var repeatedBtn = document.querySelector(".repeatedBtn");
+    var blankBtn = document.querySelector(".blankBtn");
+    // 3. 获取编码节点
+    var code01 = document.querySelector(".code01");
+    var code02 = document.querySelector(".code02");
+    var code03 = document.querySelector(".code03");
+    var code04 = document.querySelector(".code04");
+    var code05 = document.querySelector(".code05");
+    var code06 = document.querySelector(".code06");
+    // 4.获取提示信息节点
+    var message = document.querySelector(".message");
+    // 5.获取昵称显示节点
+    var specialShow = document.querySelector(".specialShow");
+    var repeatedShow = document.querySelector(".repeatedShow");
+    var blankShow = document.querySelector(".blankShow");
+    
+    // 检测
+})();
